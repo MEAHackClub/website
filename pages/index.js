fa19ce55da3b9f5c33dddab2ccdd71bb2e24c715
Shawn Zhuang <cubershz@gmail.com>
2021-01-31 20:01:14 -0800

    make homepage mobile friendly

--- pages/index.js
+++ pages/index.js
@@ -15,17 +15,17 @@ export default function Page(){
                 <Image height="100" width="100" src="images/logo.png"/>
             </Container>
             <Box sx={{ bg: 'sheet', color:'text'}} as="header">
-                <Container sx={{ paddingTop: 5, paddingBottom: [3, 4], textAlign: 'center' }}>
-                    <Heading as="h1" color="red" variant="title" >
+                <Container sx={{ paddingTop: [6,6,6,5,5], paddingBottom: [3, 4], textAlign: 'center' }}>
+                    <Heading sx={{fontSize:[7, 7, 7, 6, 6]}} as="h1" color="red" variant="title" >
                         MEA Hack Club
                     </Heading>
-                    <Text sx={{ paddingTop: '3', display:'flex',alignItems:'center',justifyContent:'center' }}>
+                    <Text sx={{ fontSize:[5, 5, 5, 3, 3], paddingTop: '3', display:'flex',alignItems:'center',justifyContent:'center',flexDirection:['column','column','column','row','row'] }}>
                         <NavLink href="https://github.com/MEA-Hack-Club" sx={{ m:'10px'}}>GitHub</NavLink>
                         <NavLink href="/projects" sx={{ m:'10px'}}>Projects</NavLink>
                     </Text>
                 </Container>
             </Box>
-            <Container sx={{ textAlign:'left' }}>
+            <Container sx={{ mt:'5', textAlign:'left' }}>
                 <Content/>
             </Container>
             
