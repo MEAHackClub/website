4287ea1ada562ecf2d63a3f00d0f9d0cd3662984
Shawn Zhuang <cubershz@gmail.com>
2021-02-20 13:54:51 -0800

    add some content to mea platformer

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet'
 export default function Header({ children }){
     return (
         <Helmet>
-            <title>Projects | MEA Hack Club</title>
+            <title>{children}</title>
             <link rel="shortcut icon" href="/static/favicon.ico"/>
         </Helmet>
     )
