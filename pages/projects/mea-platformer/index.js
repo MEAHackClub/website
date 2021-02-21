276604c77761ccbfd19a8e3bb935e56b7dcb3352
Shawn Zhuang <cubershz@gmail.com>
2021-02-20 23:35:59 -0800

    hmm whats happening with vercel

--- pages/projects/mea-platformer/index.js
+++ pages/projects/mea-platformer/index.js
@@ -8,7 +8,7 @@ export default function Page(){
             header="MEA Platformer"
             title="MEA Platformer Project"
         >
-        <Content/>
+            <Content/>
         </ProjectsPage>
     )
 }
