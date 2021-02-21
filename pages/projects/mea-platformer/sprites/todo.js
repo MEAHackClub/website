cd51926fb86db49cee4fb23be62ad8b7b932b23b
Shawn Zhuang <cubershz@gmail.com>
2021-02-20 23:22:39 -0800

    fix sprites todo

--- pages/projects/mea-platformer/sprites/todo.js
+++ pages/projects/mea-platformer/sprites/todo.js
@@ -1,5 +1,5 @@
 import ProjectsPage from '../../../../components/projects-page'
-import ProjectCard from '../../../../components/project-card'
+import { Grid, Text, Card } from 'theme-ui'
 // import Content from './todo-sprites.mdx'
 
 export default function Page(){
@@ -10,36 +10,61 @@ export default function Page(){
             header="MEA Platformer - Pixel Art To Do"
             title="MEA Platformer - Pixel Art To Do"
         >
-            <ul sx={{
-                display: 'flex', flexDirection: "row" 
-            }}>
-                <li><ProjectCard>
-                    Sprites
-
-                    <ol>
-                        <li> Main Character (AJ) </li>
-                        <li> Enemy </li>
-                        <li> Obstacles (AJ) </li>
-                    </ol>
-                </ProjectCard></li>
-
-                <li><ProjectCard>
-                    Tilemaps
-
-                    <ol>
-                        <li> Icy road (Blake)</li>
-                    </ol>
-                </ProjectCard></li>
-
-                <li><ProjectCard>
-                    Background art
-
-                    <ol>
-                        <li> Icy mountain (Kaeleb) </li>
-                        <li> MEA Campus </li>
-                    </ol>
-                </ProjectCard></li>
-            </ul>
+            <Grid width="50%" columns="2">
+                <ElementCard
+                    title="Sprites"
+                    inProgress={["Main character (AJ)", "Enemy", "Obstacles (AJ)"]}
+                />
+                <ElementCard
+                    title="Tilemaps"
+                    inProgress={["Icy road (Blake)"]}
+                />
+                <ElementCard
+                    title="Background art"
+                    inProgress={["Icy mountain (Kaeleb)", "MEA Campus"]}
+                />
+            </Grid>
         </ProjectsPage>
+    )
+}
+
+function ElementCard({ title, inProgress, done}){
+    return (
+        <Card sx={{ 
+            backgroundImage: t => t.util.gx('cyan', 'blue'),
+            width: '50%'
+        }}>
+            <Text variant="headline">
+                {title}
+            </Text>
+            {inProgress && <div>
+                <Text variant="subheadline">
+                    In progress
+                </Text>
+                <ol>
+                    {inProgress.map(element => {
+                        return <Text variant="subheading">
+                            <li>
+                                {element}
+                            </li>
+                        </Text>
+                    })}
+                </ol>
+            </div> }
+            {done && <div>
+                <Text variant="subheadline">
+                    Done
+                </Text>
+                <ol>
+                    {done.map(element => {
+                        return <Text variant="subheading">
+                            <li>
+                                {element}
+                            </li>
+                        </Text>
+                    })}
+                </ol>
+            </div> }
+        </Card>
     )
 }
