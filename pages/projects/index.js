a63088f32a99896b63366496b7061c15f5bd4197
Shawn Zhuang <cubershz@gmail.com>
2021-02-21 13:18:24 -0800

    rip off hack club's workshopCard

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,54 @@
-import { Card, Text,Heading, Button, Container, Box, IconButton, NavLink } from 'theme-ui';
+import { Card, Text,Heading, Button, Link, Container, Box, IconButton, NavLink } from 'theme-ui';
 import { Helmet } from 'react-helmet';
-import ProjectCard from '../../components/project-card'
 import BackButton from '../../components/back-button'
 import Header from '../../components/header'
+
+const ProjectCard = ({ slug, name, description, img, section }) => (
+  <Link sx={{ textDecoration: 'none' }} href={`/projects/${slug}`} passHref>
+    <Card
+      as="a"
+      variant="interactive"
+      sx={{
+        color: 'text',
+        textDecoration: 'none',
+        p: [0, 0],
+        lineHeight: 0,
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <Box sx={{ p: 3, lineHeight: 'body' }}>
+        <Heading as="h3" sx={{ my: 1 }}>
+          {name}
+        </Heading>
+        <Text variant="caption">{description}</Text>
+      </Box>
+      {['starters', 'web', 'bounties', 'hack_island'].includes(section) && (
+        <Box
+          sx={{
+            width: '100%',
+            mt: 'auto',
+            ...(img && { height: 0, paddingBottom: '50%' }),
+            '> img': { objectFit: 'cover', objectPosition: 'center' },
+            '@media print': { display: 'none' }
+          }}
+        >
+          {/* currently Next Images only support known domains, so we can’t use them for bounties/etc with the `img` in metadata */}
+          {img ? (
+            <Image alt={`${name} demo`} src={img} loading="lazy" />
+          ) : (
+            <NextImage
+              alt={`${name} demo`}
+              src={`/content/workshops/${slug}/img/demo.png`}
+              width={512}
+              height={256}
+            />
+          )}
+        </Box>
+      )}
+    </Card>
+  </Link>
+)
 
 export default function Page(){
     return(
@@ -13,27 +59,18 @@ export default function Page(){
             <BackButton href="/">
                 Home
             </BackButton>
-            <Box sx={{ mt: '4', textAlign: 'center' }}>
+            <Box sx={{ mt: '4', }}>
                 <Container>
-                    <Heading as="h1" variant="title" sx={{ mb: 4 }}>
+                    <Heading as="h1" variant="headline" sx={{ mb: 4 }}>
                         Current projects
                     </Heading>
-                    <Container sx={{ display:'flex', justifyContent:'space-evenly', textAlign:'left'}}>
-                        <ProjectCard href="/projects/mea-platformer">
-                            MEA Platformer Game (in progress)
-                        </ProjectCard>
-                    </Container>
-                    <Heading as="h1" variant="title" sx={{ m: 4 }}>
+                        <ProjectCard slug="mea-platformer" name="MEA Platformer Game" description="description" section="current-projects"/>
+                    <Heading as="h1" variant="headline" sx={{ mb: 4 }}>
                         Past projects
                     </Heading>
-                    <Container sx={{ display:'flex', justifyContent:'space-evenly', textAlign:'left'}}>
-                        <ProjectCard href="/projects/dungeon-crawler">
-                            Python Dungeon Crawler
-                        </ProjectCard>
-                        <ProjectCard href="/projects/cyoa">
-                            Choose Your Own Adventure Game
-                        </ProjectCard>
-                    </Container>
+                        <ProjectCard slug="dungeon-crawler" name="Dungeon Crawler" description="A workshop from Hack Club"/>
+                        <br/>
+                        <ProjectCard slug="cyoa" name="Choose your own adventure game" description="A text based python game"/> 
                 </Container>
             </Box>
         </div>
