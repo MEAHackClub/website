de8914d0f1cedd7c9bf82cfb994e55810e414d7a
Shawn Zhuang <cubershz@gmail.com>
2021-03-04 11:27:32 -0800

    add to todo list

--- pages/projects/mea-platformer/todo.js
+++ pages/projects/mea-platformer/todo.js
@@ -13,6 +13,22 @@ const cards = [
     {
         title: "Add projectile to player",
     },
+    {
+        title: "Add health to player",
+        inProgress: [
+            "current health (property)", 
+            "max health (property)", 
+            "health bar length (property)",
+            "damage function that decreases player health",
+            "heal function that increases player health",
+            "function that draws the health bar (hint: try using pygame.draw.rect :))",
+            "enemy doing damage to player on collision (hint: check moveRoutine() in enemy.py)",
+            "function that heals the player a little bit every 2 seconds (hint: pygame.time.get_ticks() for ms passed)",
+        ],
+        done: [
+
+        ]
+    }
     
 ]
 
