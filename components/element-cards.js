1fdc18a60af7c52ca37d821dd4f39da1f30d33ac
Shawn Zhuang <cubershz@gmail.com>
2021-03-10 22:16:23 -0800

    planning for 3-11

--- components/element-cards.js
+++ components/element-cards.js
@@ -10,30 +10,45 @@ export default function ElementCards({ cards }){
     )
 }
 
-function ElementCard({ title, inProgress, done}){
+function ElementCard({ description, title, inProgress, done}){
+    let titleColor = inProgress||!done ? 'blue' : 'red';
     return (
         <Card variant="interactive" sx={{ 
             // backgroundImage: t => t.util.gx('cyan', 'blue'),
         }}>
-            <Text variant="headline" sx={{ color:'blue' }}>
+            <Text variant="headline" sx={{ color:titleColor }}>
                 {title}
             </Text>
+            {description && <Text variant="caption" sx={{ mb: 1 }}>
+                {description}
+            </Text>}
             {inProgress && <div>
-                <Text variant="subheadline">
+                <Text variant="subheadline" sx={{ color: 'blue' }}>
                     In progress
                 </Text>
                 <ol>
                     {inProgress.map(element => {
-                        return <Text variant="subheading">
+                        const isString = typeof(element)==="string";
+                        const item = isString ? element : element.item
+                        return (<div>
+                        {<Text variant="subheading">
                             <li>
-                                {element}
+                                {item}
+                                <ul>
+                                    {element.subitems && element.subitems.map(subitem => {
+                                        return (<li>
+                                            {subitem}
+                                        </li>)
+                                    })}
+                                </ul>
                             </li>
-                        </Text>
+                        </Text>}
+                        </div>)
                     })}
                 </ol>
             </div> }
             {done && <div>
-                <Text variant="subheadline">
+                <Text variant="subheadline" sx={{ color: 'red'}}>
                     Done
                 </Text>
                 <ol>
--- pages/projects/mea-platformer/todo.js
+++ pages/projects/mea-platformer/todo.js
@@ -4,8 +4,45 @@ import { Grid } from 'theme-ui'
 
 const cards = [
     {
+        title: "Add ball projectile",
+        description: "Create a ball-shaped projectile fired from the player. If the player is going right, fire right, and the same with player moving left. If the player is stationary, fire upwards.",
+        inProgress: [
+            "Make new projectile object (class)",
+            {item: "Add properties to object. Have each variable be passed in from the constructor (init() function)", subitems: [
+                "x position",
+                "y position",
+                "radius",
+                "color",
+                "direction (-1 = left, 1 = right)",
+                "velocity (speed. Have this be 8 times the direction, so don't add this in init)",
+            ]}, 
+            {item: "draw function", subitems: [
+                "pass in the display as a parameter",
+                "pygame.draw.circle function",
+            ]},
+            {item: "move function", subitems: [
+                "add velocity to x position",
+            ]},
+            {item: "add properties to player", subitems: [
+                "add projectiles property (initialize to empty array)",
+            ]},
+            {item: "add move_projectiles function to player", subitems: [
+                "projectile.move() on each of the projectiles",
+                "projectile.draw() each of projectiles (make sure to pass in display as a parameter)",
+                "if projectile x position is greater than 400 or less than 0, erase it from array (out of screen)",
+            ]},
+            {item: "add to main game loop", subitems: [
+                "add a projectile to player.projectiles on spacebar (KEYDOWN and K_SPACE. create a projectile at player.rect.x and player.x.y)",
+                "run player.move_projectiles in game loop to move projectiles every loop",
+            ]},
+        ],
+    },
+    {
         title: "Camera Follow",
-        inProgress: ["Basic camera follow", "Parallax scrolling"]
+        inProgress: [
+            "Basic camera follow", 
+            "Parallax scrolling"
+        ],
     },
     {
         title: "Make more levels",
@@ -14,14 +51,15 @@ const cards = [
         title: "Add projectile to player",
     },
     {
-        title: "Fix player movement bug",
-    },
-    {
         title: "Enemy movement",
         inProgress: [
             "Make movement more customizable",
             "Follow the player like zombie",
         ]
+    },
+    {
+        title: "Fix player movement bug",
+        done: ["separated direction variable to moving left and right"],
     },
     {
         title: "Add health to player",
