fefd6195aae141ce1ccd6b5f40495d7438d511b4
Shawn Zhuang <cubershz@gmail.com>
2021-03-10 18:20:03 -0800

    deploy last week's changes

--- pages/projects/mea-platformer/todo.js
+++ pages/projects/mea-platformer/todo.js
@@ -4,22 +4,6 @@ import { Grid } from 'theme-ui'
 
 const cards = [
     {
-        title: "Add health to player",
-        inProgress: [
-            "current health (property)", 
-            "max health (property)", 
-            "health bar length (property)",
-            "damage function that decreases player health",
-            "heal function that increases player health",
-            "function that draws the health bar (hint: try using pygame.draw.rect :))",
-            "enemy doing damage to player on collision (hint: check moveRoutine() in enemy.py)",
-            "function that heals the player a little bit every 2 seconds (hint: pygame.time.get_ticks() for ms passed)",
-        ],
-        done: [
-
-        ]
-    },
-    {
         title: "Camera Follow",
         inProgress: ["Basic camera follow", "Parallax scrolling"]
     },
@@ -33,9 +17,23 @@ const cards = [
         title: "Fix player movement bug",
     },
     {
-        title: "Enemy movemeny",
+        title: "Enemy movement",
         inProgress: [
             "Make movement more customizable",
+            "Follow the player like zombie",
+        ]
+    },
+    {
+        title: "Add health to player",
+        done: [
+            "damage function that decreases player health",
+            "heal function that increases player health",
+            "function that draws the health bar (hint: try using pygame.draw.rect :))",
+            "enemy doing damage to player on collision (hint: check moveRoutine() in enemy.py)",
+            "function that heals the player a little bit every 2 seconds (hint: pygame.time.get_ticks() for ms passed)",
+            "current health (property)", 
+            "max health (property)", 
+            "health bar length (property)",
         ]
     },
 ]
